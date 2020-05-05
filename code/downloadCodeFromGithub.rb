8846a25e1c765381c448a65c46cacdf9958d03cf
Daniel Vela <veladan@me.com>
2020-05-05 19:49:26 +0200

    fix download

--- code/downloadCodeFromGithub.rb
+++ code/downloadCodeFromGithub.rb
@@ -6,9 +6,16 @@ require "open-uri"
 require "fileutils"
 
 def download(url, path)
-  case io = URI.open(url)
-  when StringIO then File.open(path, 'w') { |f| f.write(io) }
-  when Tempfile then io.close; FileUtils.mv(io.path, path)
+  uri = URI(url)
+  Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+    request = Net::HTTP::Get.new uri
+    http.request request do |response|
+      open path, 'w' do |io|
+        response.read_body do |chunk|
+          io.write chunk
+        end
+      end
+    end
   end
 end
 
